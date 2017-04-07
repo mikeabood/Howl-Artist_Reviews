f9792513a4842043a4cda117c84ffefddb5f4714
Michael Abood <Abood@Mikes-Macbook.local>
2017-04-07 11:11:16 -0500

    update

--- controllers/AppController.rb
+++ controllers/AppController.rb
@@ -4,24 +4,24 @@ class AppController < Sinatra::Base
 	require 'bundler'
 	Bundler.require
 
-# ActiveRecord::Base.establish_connection(
-# 		:adapter => 'postgresql',
-# 		:database => 'howl'
-# 		)
+ActiveRecord::Base.establish_connection(
+		:adapter => 'postgresql',
+		:database => 'howl'
+		)
 
-db = URI.parse(ENV['DATABASE_URL'])
+# db = URI.parse(ENV['DATABASE_URL'])
 
-  DB_NAME = db.path[1..-1]
+#   DB_NAME = db.path[1..-1]
 
-  ActiveRecord::Base.establish_connection(
-    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-    :host     => db.host,
-    :port     => db.port,
-    :username => db.user,
-    :password => db.password,
-    :database => DB_NAME,
-    :encoding => 'utf8'
-  )
+#   ActiveRecord::Base.establish_connection(
+#     :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+#     :host     => db.host,
+#     :port     => db.port,
+#     :username => db.user,
+#     :password => db.password,
+#     :database => DB_NAME,
+#     :encoding => 'utf8'
+#   )
 	
 
 
